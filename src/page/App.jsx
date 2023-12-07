4decb2e2827be6bd9f29567fc11049e71ced7e31
Danilo Souza <danilobrayann@gmail.com>
2023-12-07 20:31:28 -0300

    fix: correção na escrição no  index

--- src/page/App.jsx
+++ src/page/App.jsx
@@ -1,7 +1,7 @@
 import Header from "../components/header"
 import Home from "../components/Home"
 import Sobre from "../components/sobre"
-import Projetos from "../components/Projetos/inde"
+import Projetos from "../components/Projetos/index"
 function App() {
 
 
